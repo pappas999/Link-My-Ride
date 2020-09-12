c6efe2f2271c3d79de2c7728e3b8726a54787322
pappas999 <harry@genesisblockchain.com.au>
2020-09-12 22:28:32 +0930

    send eth passed in newRentalAgreement to generated contract
    send eth passed in newRentalAgreement to generated contract

--- contracts/Rental-Agreements.sol
+++ contracts/Rental-Agreements.sol
@@ -50,9 +50,9 @@ contract RentalAgreementFactory {
     
     constructor() public payable {
         //this code adds a vehicle so we don't have to keep doing it manually as part of development, then it creates a simple contract for the vehicle
-        newVehicle(0x54a47c5e6a6CEc35eEB23E24C6b3659eE205eE35,123,'sadfasfasdfsda',0.1 * 1 ether,1 ether,VehicleModels.Model_S,'harrys car');
-        newVehicle(0x20442A67128F4a2d9eb123e353893c8f05429AcB,567,'test',0.1 * 1 ether,1 ether,VehicleModels.Model_X,'second car');
-        newRentalAgreement(0x54a47c5e6a6CEc35eEB23E24C6b3659eE205eE35,0xaF9aA280435E8C13cf8ebE1827CBB402CE65bBf7,1599565516,1599569916,100000000000000000,1000000000000000000);
+        newVehicle(0x54a47c5e6a6CEc35eEB23E24C6b3659eE205eE35,123,'sadfasfasdfsda',0.01 * 0.01 ether,0.01 ether,VehicleModels.Model_S,'harrys car');
+        newVehicle(0x20442A67128F4a2d9eb123e353893c8f05429AcB,567,'test',0.01 * 0.1 ether,0.01 ether,VehicleModels.Model_X,'second car');
+        newRentalAgreement(0x54a47c5e6a6CEc35eEB23E24C6b3659eE205eE35,0xaF9aA280435E8C13cf8ebE1827CBB402CE65bBf7,1599565516,1599569916,1000000000000000,10000000000000000);
     }
     
 
@@ -85,7 +85,7 @@ contract RentalAgreementFactory {
        require (msg.value >= _totalRentCost.add(_totalBond),'Incorrect rent & bond paid');
         
        //create new Rental Agreement
-       RentalAgreement a = new RentalAgreement(_vehicleOwner, _renter, _startDateTime, _endDateTime, _totalRentCost, _totalBond,  
+       RentalAgreement a = (new RentalAgreement).value(_totalRentCost.add(_totalBond))(_vehicleOwner, _renter, _startDateTime, _endDateTime, _totalRentCost, _totalBond,  
                                                  LINK_ROPSTEN, ORACLE_CONTRACT, ORACLE_PAYMENT, job_id);
        
        //get price of ETH from price feeds to be used in calculations. or do it in web3 before solidity
@@ -447,7 +447,9 @@ contract RentalAgreement is ChainlinkClient, Ownable  {
          //In this case, we approve. Contract becomes Rejected. No more actions should be possible on the contract in this status
          //return money to renter
          renter.transfer(address(this).balance);
+         //return any LINK tokens in here back to the DAPP wallet
          agreementStatus = RentalAgreementFactory.RentalAgreementStatus.REJECTED;
+         
      }
      
    /**
