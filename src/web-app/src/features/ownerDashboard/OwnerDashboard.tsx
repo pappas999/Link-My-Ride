5eaf904dfaedc3fa68e9292c029e35113472c3dc
MATTOP\Matthew <m.durkin92@gmail.com>
2020-09-13 00:05:24 +0100

    Style changes

--- src/web-app/src/features/ownerDashboard/OwnerDashboard.tsx
+++ src/web-app/src/features/ownerDashboard/OwnerDashboard.tsx
@@ -1,8 +1,15 @@
 import React from "react"
+import styled from "styled-components"
 import { MyVehicle } from "./MyVehicle"
 import { MyRentalContracts } from "../rentalContracts"
 
 export const OwnerDashboard = () => <>
     <MyVehicle />
+    <StyledHr />
     <MyRentalContracts asOwner={true} />
 </>
+
+const StyledHr = styled.hr`
+    width: 100%;
+    max-width: ${({ theme }) => theme.typography.pxToRem(800)};
+`
--- src/web-app/src/features/renterDashboard/RenterDashboard.tsx
+++ src/web-app/src/features/renterDashboard/RenterDashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 import { MyRentalContracts } from "../rentalContracts"
 import { Link } from "react-router-dom"
 import { Button } from "@material-ui/core"
@@ -10,5 +11,11 @@ export const RenterDashboard = () => <>
     >
         Rent a car!
         </Button>
+    <StyledHr />
     <MyRentalContracts asOwner={false} />
 </>
+
+const StyledHr = styled.hr`
+    width: 100%;
+    max-width: ${({theme}) => theme.typography.pxToRem(800)};
+`
