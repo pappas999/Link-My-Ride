1acfea9be487e336aae3de22349fc2a083c9831b
MATTOP\Matthew <m.durkin92@gmail.com>
2020-10-01 01:13:42 +0100

    Replace images on home page

--- src/web-app/src/features/home/Home.tsx
+++ src/web-app/src/features/home/Home.tsx
@@ -45,7 +45,7 @@ const Content = styled.div`
 `
 
 const Background = styled.div`
-  min-height: max(calc(100vh - 40rem), 32rem);
+  min-height: max(calc(100vh - 40rem), 34rem);
   height: 100%;
   background: rgba(36,93,232,1);
   background: linear-gradient(0deg, rgba(21,57,145,1) 0%, rgba(36,93,232,1) 100%);
